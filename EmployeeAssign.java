62041242abd17dba2b049ec51f8e4ee49b89ff63
NehaSurakanti <surakantineha@gmail.com>
2023-05-06 19:29:13 +0530

    Assignment Menu driven write to file and read to file

--- EmployeeAssign.java
+++ EmployeeAssign.java
@@ -16,6 +16,7 @@ abstract class Employee
 		System.out.println("Enter name for "+designation);
 		name=sc.nextLine();
 		age=InvalidAgeException.readAge();
+		writeToFile();
 		count++;
 	}
 	public void raiseSalary()
@@ -28,6 +29,50 @@ abstract class Employee
 	public final void display()
 	{
 		System.out.println("Name:"+name+" Age:"+age+" Salary:"+salary+" Designation:"+designation);
+	}
+
+	public static void readFromFile()
+	{
+		try
+		{
+			File f=new File("D:/Target/Assignments/employee/employees.csv");
+			BufferedReader fr=new BufferedReader(new FileReader("D:/Target/Assignments/AssignmentLocal/employee/employees.csv"));
+			String line=null;
+			System.out.println();
+			while((line=fr.readLine())!=null)
+			{
+				StringTokenizer st=new StringTokenizer(line,",");
+				int tokens=st.countTokens();
+				System.out.println("Name:"+st.nextToken());
+				System.out.println("Age:"+st.nextToken());
+				System.out.println("Salary:"+st.nextToken());
+				System.out.println("Designation:"+st.nextToken());
+				System.out.println("-----------------------");						
+			}
+			fr.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public void writeToFile()
+	{
+		try
+		{
+			File dir1=new File("employee");
+			dir1.mkdir();
+			File f=new File(dir1,"employees.csv");
+			f.createNewFile();
+			PrintWriter pw=new PrintWriter(new FileOutputStream(f,true));
+			pw.write(name+","+age+","+salary+","+designation+"\n");
+			pw.flush();
+			pw.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 	}
 }
 final class Tester extends Employee
@@ -87,25 +132,34 @@ class InvalidAgeException extends RuntimeException
 }
 public class EmployeeAssign
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
-		System.out.println("Enter Tester details:");
-		Employee e1=new Tester();
-		System.out.println("Enter Programmer details:");
-		Employee e2=new Programmer();
-		System.out.println("Enter Manager details:");
-		Employee e3=new Manager();
-		e1.display();
-		e2.display();
-		e3.display();
-		System.out.println("-----------------");
-		System.out.println("After raising:");
-		System.out.println("-----------------");
-		e1.raiseSalary();
-		e2.raiseSalary();
-		e3.raiseSalary();
-		e1.display();
-		e2.display();
-		e3.display();
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		while(true)
+		{
+			System.out.println("Enter your choice:\n1.Create\n2.Display\n3.Exit");
+			int ch=Integer.parseInt(br.readLine());
+			switch(ch)
+			{
+				case 1:boolean c=true;
+					while(c)
+					{
+						System.out.println("Enter your choice:\n1.Tester\n2.Programmer\n3.Manager\n4.Exit");
+						int ch1=Integer.parseInt(br.readLine());
+						switch(ch1)
+						{
+							case 1:Employee e1=new Tester();break;
+							case 2:Employee e2=new Programmer();break;
+							case 3:Employee e3=new Manager();break;
+							case 4:c=false;break;
+							default:System.out.println("Wrong choice");
+						}
+					}break;
+				case 2:Employee.readFromFile();
+					break;
+				case 3:System.exit(0);break;
+				default:System.out.println("Wrong choice");
+			}
+		}	
 	}
 }
